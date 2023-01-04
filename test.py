2ef594a81787fc7fd61e07710f04b45d62f7760b
SupKamatrip <raphael.ancelin@epsi.fr>
2023-01-04 20:28:10 +0100

    Rajout de commentaires start

--- test.py
+++ test.py
@@ -4,22 +4,20 @@ import sys
 #import googletrans
 
 
-weathercity = input("Météo pour quelle  ville ? : ")
+weathercity = input("Météo pour quelle  ville ? : ") # input va nous permettre de saisir le contenu de weathercity
 weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q='+weathercity+'&appid=9db93bdd1a0d2f47617d2c9158a903fa')
 url = ('http://api.openweathermap.org/data/2.5/weather?q='+weathercity+'&appid=9db93bdd1a0d2f47617d2c9158a903fa')
-
-
-
+# on récupère ci-dessus les données de la requetes web
 
 def spinning_cursor():  # Scimulation d'un temp de hcargement
     while True:
         for cursor in '|/-\\':
-            yield cursor
+            yield cursor  # yield retourne le générateur cursor ( au lieu de stocker on récupère l'info à la volée )
+                          # s'éxécute qu'une seule fois
 
+data = weather.json()  # liste en java de weather ( notre requete est stockée dans une liste)
 
-data = weather.json()
-
-gps = data['coord']
+gps = data['coord']  # retourne les coordonnées gps de la station météo
 temp = data['main']['temp']
 print(gps)
 description = data['weather'][0]['description']
@@ -31,7 +29,7 @@ for _ in range(25):
     time.sleep(0.1)
     sys.stdout.write('\b')
 
-convert = int(temp - 273.15)
+convert = int(temp - 273.15)  # convertir les fahrenheit en celsius
 
 #from googletrans import  Translator
 #trad = Translator()
